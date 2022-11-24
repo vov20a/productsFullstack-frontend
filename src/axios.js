fb1588bcf0688e46b5a5c3d311da76cfa9f2136e
Petr <vov20a@mail.ru>
2022-11-24 12:02:47 +0300

    REACT_API

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4444',
+  // baseURL: 'http://localhost:4444',
+  baseURL: REACT_APP_API_URL,
 });
 
 instance.interceptors.request.use((config) => {
